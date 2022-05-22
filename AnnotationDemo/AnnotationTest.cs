e22e6c9f175f191b5ca731292e7312747637635c
Imransk7768 <imransk7768@gmail.com>
2022-05-23 00:58:50 +0530

    [Imran] Added: Print Annotation Demo Program

---
+++ AnnotationDemo/AnnotationTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnotationDemo
+{
+    public class AnnotationTest
+    {
+        public static void Display()
+        {
+            Console.WriteLine("Employee Class Validation");
+            Employee objemployee = new Employee();
+
+            Console.Write("Enter Name : ");
+            objemployee.Name = Console.ReadLine();
+            Console.Write("Enter Age : ");
+            objemployee.Age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Mobile : ");
+            objemployee.MobileNumber = Convert.ToInt64(Console.ReadLine());
+            Console.Write("Enter Email : ");
+            objemployee.Email = Console.ReadLine();
+
+            ValidationContext context = new ValidationContext(objemployee, null, null);
+            List<ValidationResult> results = new List<ValidationResult>();
+            bool valid = Validator.TryValidateObject(objemployee, context, results, true);
+
+            if (!valid)
+            {
+                foreach (ValidationResult totalresult in results)
+                {
+                    Console.WriteLine("Member Name : {0}", totalresult.MemberNames.First(), Environment.NewLine);
+                    Console.WriteLine("Error Message : : {0}{1}", totalresult.ErrorMessage, Environment.NewLine);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Name : " + objemployee.Name + "\nAge : " + objemployee.Age + "\nMobile : " + objemployee.MobileNumber + "\nEmail : " + objemployee.Email);
+            }
+            Console.WriteLine("Press Any Key To Exit.");
+        }
+    }
+}
---
+++ AnnotationDemo/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnotationDemo
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "Employee {0} is Required")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Name Should be above 3 Char")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email should be Required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Range(18, 99, ErrorMessage = "Age Should Be Above 18")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Mobile should be Required")]
+        [DataType(DataType.PhoneNumber)]
+        public long MobileNumber { get; set; }
+    }
+}
